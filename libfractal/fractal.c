d1e84b31cf925abcdaa9e93bce93e2706cad1b84
pablo <pgonzalezalv@users.noreply.github.com>
2016-04-20 11:20:09 +0200

    fix: missing free for value array

--- libfractal/fractal.c
+++ libfractal/fractal.c
@@ -4,23 +4,24 @@
 struct fractal *fractal_new(const char *name, int width, int height, double a, double b)
 {
     struct fractal *f_new = malloc(sizeof(struct fractal));
-    
+
     int i = 0;
     for(i = 0; name[i] != '\0'; i++)
         f_new->name[i] = name[i];
     f_new->name[i] = '\0';
-    
+
     f_new->value = (int *)malloc(height * width * sizeof(int));
     f_new->w = width;
     f_new->h = height;
     f_new->a = a;
     f_new->b = b;
-    
+
     return NULL;
 }
 
 void fractal_free(struct fractal *f)
 {
+    free(f->value);
     free(f);
 }
 
