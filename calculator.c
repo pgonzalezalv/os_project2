e946191d2ec064bbfe172da400ec73ac36704596
olewicki <doriane.olewicki@gmail.com>
2016-05-01 22:35:33 +0200

    fix : calculator

--- calculator.c
+++ calculator.c
@@ -1,23 +1,38 @@
 #include "calculator.h"
 #include "libfractal/fractal.h"
 
-int calculator()
+int calculator(fractal_t *f, int currentAve)
 {
 
 	int x = 0;
 	int y = 0;
-	int width = 0;
-	int height = 0;
-	fractal_t *toCompute = NULL;
+	int width = fractal_get_width(f);;
+	int height = fractal_get_height(f);
 
-	toCompute = dequeue();
-	width = toCompute->width
-	height = toCompute->height
+	double sum = 0;
+	int count = 0;
 
-	for ( x = 0; x < width ; x++) {
-		for ( y = 0; y < height ; y++) {
-			fractal_compute_value(toCompute, x, y);
+	int value;
+	double average;
+
+	//fractal_t *toCompute = NULL;
+
+	//toCompute = dequeue(); 							// mettre ca dans le main
+	//width = toCompute->width
+	//height = toCompute->height
+
+	for ( y = 0; y < height ; y++) {
+		for( x = 0; x < width ; x++)  {
+			value = fractal_compute_value(toCompute, x, y);
+			fractal_set_value(f, value);
+			sum += value;
+			count++;
 		}
+	}
+
+	if (currentAve < average)
+	{
+		//change fractal_fav
 	}
 
 	return 0;
